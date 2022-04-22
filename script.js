5055675c016f826f15c89ee73be5c52f82d3b986
skywalktrash <103572940+skywalktrash@users.noreply.github.com>
2022-04-22 18:01:55 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Hi\nPress OK button");
+var nameUser=prompt("Введите ваше имя")
+if(nameUser) {
+    alert(nameUser + ", добро пожаловать на сайт!");
+}
+else {
+    alert("Ошибка! Введите имя!")
+    var nameuser2 = prompt("Введите имя: ")
+    alert(nameuser2 + ", добро пожаловать!")
+}
